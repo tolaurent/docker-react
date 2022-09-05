6b52eeda3d24764b8a4cb9d996eacd4b37d8705b
Thomas Laurent <12704759+tolaurent@users.noreply.github.com>
2022-09-05 16:50:37 +0900

    Changed the message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cool !
+          This was changed to feature branch
         </p>
         <a
           className="App-link"
